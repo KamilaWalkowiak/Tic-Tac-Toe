7e64347fddc416ee6d10cb7eedd446dc1a3f45df
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-03-15 22:46:37 +0100

    An Interactive Component

--- tutorial/src/Square.js
+++ tutorial/src/Square.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'dupa'})}>
+                {this.state.value}
             </button>
         );
     }
