60bf15e0a2453397480043cdb0ed827fa6795ada
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-03-16 23:16:08 +0100

    Implementing Time Travel.

--- tutorial/src/Game.js
+++ tutorial/src/Game.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import Board from './Board'
 
 class Square extends PureComponent {
@@ -9,18 +9,16 @@ class Square extends PureComponent {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
 
     handleClick(i) {
-        const history = this.state.history;
-        console.log('1',history);
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        console.log(current);
         const squares = current.squares.slice();
-        console.log(squares);
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
@@ -29,21 +27,31 @@ class Square extends PureComponent {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
-        }, () => {console.log('2',this.state.history)});
+        });
+
     }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
 
     render() {
 
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         const moves = history.map((step, move) => {
             const desc = move ?
                 'Go to move #' + move :
                 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
